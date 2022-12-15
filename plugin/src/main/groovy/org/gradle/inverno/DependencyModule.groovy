95078ab7e1209c157852cb71b603196c4121dfee
Gary Hale <gary@gradle.com>
2022-12-15 16:24:16 -0500

    Current state

--- plugin/src/main/groovy/org/gradle/inverno/DependencyModule.groovy
+++ plugin/src/main/groovy/org/gradle/inverno/DependencyModule.groovy
@@ -3,11 +3,11 @@ package org.gradle.inverno
 import java.lang.module.ModuleReference
 import java.nio.file.Path
 
-public class DependencyModule {
+class DependencyModule {
 
-    private final Path path;
+    final Path path;
 
-    private final ModuleReference moduleReference;
+    final ModuleReference moduleReference;
 
     DependencyModule(Path path, ModuleReference moduleReference) {
         this.path = path;
--- plugin/src/main/groovy/org/gradle/inverno/ModularizeDependenciesTask.groovy
+++ plugin/src/main/groovy/org/gradle/inverno/ModularizeDependenciesTask.groovy
@@ -4,21 +4,69 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileCollection
+import org.gradle.api.file.FileSystemOperations
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
-class ModularizeDependenciesTask extends DefaultTask {
+import javax.inject.Inject
+import java.nio.file.FileSystem
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.jar.Attributes
+import java.util.jar.Manifest
 
+abstract class ModularizeDependenciesTask extends DefaultTask {
+
+
+    public static final String AUTOMATIC_MODULE_NAME = "Automatic-Module-Name"
+
+    @Inject
     @InputFiles
-    FileCollection dependencies;
+    abstract FileCollection getDependencies()
 
+    @Inject
     @OutputDirectory
-    DirectoryProperty explodedDirectory;
+    abstract DirectoryProperty getExplodedDirectory()
+
+    @Inject abstract FileSystemOperations getFilesystemOperations()
 
     @TaskAction
     void execute() {
         def dependencyModules = new ResolveDependenciesTask(dependencies).execute()
+
+        if (explodedDirectory.getAsFile().get().exists()) {
+            getFilesystemOperations().delete {
+                delete(explodedDirectory)
+            }
+        }
+
+        dependencyModules.each {dependencyModule ->
+            try (FileSystem jarFs = FileSystems.newFileSystem(URI.create("jar:" + dependencyModule.path.toUri()), Map.of())) {
+                Path manifestPath = jarFs.getPath("META-INF", "MANIFEST.MF")
+                Path moduleInfo = jarFs.getPath("module-info.class")
+                if (!Files.exists(moduleInfo)) {
+                    if (Files.exists(manifestPath)) {
+                        try (InputStream is = Files.newInputStream(manifestPath)) {
+                            Manifest manifest = new Manifest(is)
+                            if (!manifest.getMainAttributes().containsKey(AUTOMATIC_MODULE_NAME)) {
+                                manifest.getMainAttributes().put(new Attributes.Name(AUTOMATIC_MODULE_NAME), dependencyModule.moduleReference.descriptor().name());
+                                try (OutputStream jarOutput = Files.newOutputStream(manifestPath)) {
+                                    manifest.write(jarOutput);
+                                }
+                            }
+                        }
+                    } else {
+                        Manifest manifest = new Manifest();
+                        manifest.getMainAttributes().put(new Attributes.Name(AUTOMATIC_MODULE_NAME), dependency.getModuleName());
+                        try (OutputStream jarOutput = Files.newOutputStream(manifestPath)) {
+                            manifest.write(jarOutput);
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
